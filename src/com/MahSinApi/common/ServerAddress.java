29479bb10f74ff9da9f793977004d0e1cae11cbb
Your Name <you@example.com>
2022-08-28 18:29:11 +0430

    correct the server addresses

--- src/com/MahSinApi/common/ServerAddress.java
+++ src/com/MahSinApi/common/ServerAddress.java
@@ -1,6 +1,7 @@
 package com.MahSinApi.common;
 
 public class ServerAddress {
+
     public static final String MainServerAddress="http://89.32.250.218:8080/MahsinApi/";
     public static final String UPLOAD_DIRECTORY ="C:/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/MahsinApi/resources/images/";
     public static final String IMAGE_DIRECTORY=MainServerAddress+"/resources/images/";

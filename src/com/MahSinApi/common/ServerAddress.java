bb2bad5852f9bc43d1951ee8658291e1050f128b
Your Name <you@example.com>
2022-08-28 18:27:30 +0430

    correct the server addresses

--- src/com/MahSinApi/common/ServerAddress.java
+++ src/com/MahSinApi/common/ServerAddress.java
@@ -1,11 +1,11 @@
 package com.MahSinApi.common;
 
 public class ServerAddress {
-    public static final String MainServerAddress="http://192.168.1.106:8082";
-    public static final String UPLOAD_DIRECTORY ="d:/Projects/MahsinApi/web/resources/images/";
+    public static final String MainServerAddress="http://89.32.250.218:8080/MahsinApi/";
+    public static final String UPLOAD_DIRECTORY ="C:/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/MahsinApi/resources/images/";
     public static final String IMAGE_DIRECTORY=MainServerAddress+"/resources/images/";
-    public static final String UPLOAD_DIRECTORY_FOR_PARAGRAPHS_PICTURE ="d:/Projects/MahsinApi/web/resources/paragraph/";
+    public static final String UPLOAD_DIRECTORY_FOR_PARAGRAPHS_PICTURE ="C:/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/MahsinApi/resources/paragraph/";
     public static final String IMAGE_DIRECTORY_FOR_PARAGRAPHS_PICTURE=MainServerAddress+"/resources/paragraph/";
-    public static final String UPLOAD_DIRECTORY_FOR_POST ="d:/Projects/MahsinApi/web/resources/post/";
+    public static final String UPLOAD_DIRECTORY_FOR_POST ="C:/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/MahsinApi/resources/post/";
     public static final String IMAGE_DIRECTORY_FOR_POST=MainServerAddress+"/resources/post/";
 }
